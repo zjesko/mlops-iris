956b24485f910de2b219720b1850e741efb23f0c
akshatcx <akshat.c2k@gmail.com>
2021-07-01 02:11:12 +0530

    [add] comments

--- main.py
+++ main.py
@@ -3,34 +3,37 @@ from fastapi import FastAPI
 from pydantic import BaseModel
 from ml_utils import load_model, predict
 
-app = FastAPI(
-    title="Iris Predictor",
-    docs_url="/"
-)
+# defining the main app
+app = FastAPI(title="Iris Predictor", docs_url="/")
 
+# calling the load_model during startup
 app.add_event_handler("startup", load_model)
 
+# class which is expected in the payload
 class QueryIn(BaseModel):
     sepal_length: float
     sepal_width: float
     petal_length: float
     petal_width: float
 
+
+# class which is returned in the response
 class QueryOut(BaseModel):
     flower_class: str
 
 
+# Route definitions
 @app.get("/ping")
 def ping():
     return {"ping": "pong"}
 
 
 @app.post("/predict_flower", response_model=QueryOut, status_code=200)
-def predict_flower(
-    query_data: QueryIn
-):
-    output = {'flower_class': predict(query_data)}
+def predict_flower(query_data: QueryIn):
+    output = {"flower_class": predict(query_data)}
     return output
 
+
+# Main function to start the app when main.py is called
 if __name__ == "__main__":
-    uvicorn.run("main:app", host='0.0.0.0', port=8888, reload=True)
+    uvicorn.run("main:app", host="0.0.0.0", port=8888, reload=True)
--- ml_utils.py
+++ ml_utils.py
@@ -5,27 +5,22 @@ from sklearn.metrics import accuracy_score
 
 clf = GaussianNB()
 
-classes = {
-    0: "Iris Setosa",
-    1: "Iris Versicolour",
-    2: "Iris Virginica"
-}
+classes = {0: "Iris Setosa", 1: "Iris Versicolour", 2: "Iris Virginica"}
 
+# function to train and load the model during startup
 def load_model():
-	X, y = datasets.load_iris(return_X_y=True)
+    X, y = datasets.load_iris(return_X_y=True)
 
-	X_train, X_test, y_train, y_test = train_test_split(X,y, test_size=0.2)
-	clf.fit(X_train, y_train)
+    X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2)
+    clf.fit(X_train, y_train)
 
-	acc = accuracy_score(y_test, clf.predict(X_test))
-	print(f"Model trained with accuracy: {round(acc, 3)}")
+    acc = accuracy_score(y_test, clf.predict(X_test))
+    print(f"Model trained with accuracy: {round(acc, 3)}")
 
+
+# function to predict the flower using the model
 def predict(query_data):
-	x = list(query_data.dict().values())
-	prediction = clf.predict([x])[0] 
-	print(f"Model prediction: {classes[prediction]}")
-	return classes[prediction]
-
-
-
-
+    x = list(query_data.dict().values())
+    prediction = clf.predict([x])[0]
+    print(f"Model prediction: {classes[prediction]}")
+    return classes[prediction]
