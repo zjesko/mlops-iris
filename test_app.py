1cfb39ffc1247380c057c65fbf04d3627611e1cf
vinod.ad <vinod.ad@media.net>
2021-07-10 10:18:21 +0530

    Added 2 more testcases

--- test_app.py
+++ test_app.py
@@ -24,3 +24,35 @@ def test_pred_virginica():
         # asserting the correct response is received
         assert response.status_code == 200
         assert response.json() == {"flower_class": "Iris Virginica"}
+
+
+# test to check if Iris Versicolour is classified correctly
+def test_pred_versicolour():
+    # defining a sample payload for the testcase
+    payload = {
+        "sepal_length": 3,
+        "sepal_width": 1,
+        "petal_length": 1,
+        "petal_width": 2,
+    }
+    with TestClient(app) as client:
+        response = client.post("/predict_flower", json=payload)
+        # asserting the correct response is received
+        assert response.status_code == 200
+        assert response.json() == {"flower_class": "Iris Versicolour"}
+
+
+# test to check if Iris Setoda is classified correctly
+def test_pred_setosa():
+    # defining a sample payload for the testcase
+    payload = {
+        "sepal_length": 5,
+        "sepal_width": 3,
+        "petal_length": 1,
+        "petal_width": 0,
+    }
+    with TestClient(app) as client:
+        response = client.post("/predict_flower", json=payload)
+        # asserting the correct response is received
+        assert response.status_code == 200
+        assert response.json() == {"flower_class": "Iris Setosa"}
