23d3b04cfde301c882893f93b3c7b5dc7e98976c
Nikhil Gopala <gopala.nikhil@gmail.com>
2021-07-11 14:54:17 -0400

    Indentaion fixed

--- test_app.py
+++ test_app.py
@@ -23,7 +23,7 @@ def test_pred_virginica():
         response = client.post("/predict_flower", json=payload)
         # asserting the correct response is received
         assert response.status_code == 200
-       assert response.json()["flower_class"] == "Iris Virginica"
+        assert response.json()["flower_class"] == "Iris Virginica"
 
 # task 2
 
