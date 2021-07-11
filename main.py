283dafe0af4195c8fe8ef333a43ba10129edf83f
Nikhil Gopala <gopala.nikhil@gmail.com>
2021-07-11 13:35:21 -0400

    Task 4 compleete

--- main.py
+++ main.py
@@ -3,6 +3,7 @@ from fastapi import FastAPI
 from pydantic import BaseModel
 from ml_utils import load_model, predict, retrain
 from typing import List
+from datetime import date, datetime
 
 # defining the main app
 app = FastAPI(title="Iris Predictor", docs_url="/")
@@ -22,6 +23,7 @@ class QueryIn(BaseModel):
 # class which is returned in the response
 class QueryOut(BaseModel):
     flower_class: str
+    timestamp: datetime
 
 # class which is expected in the payload while re-training
 class FeedbackIn(BaseModel):
@@ -35,7 +37,7 @@ class FeedbackIn(BaseModel):
 @app.get("/ping")
 # Healthcheck route to ensure that the API is up and running
 def ping():
-    return {"ping": "pong"}
+    return {"ping": "pong", "timestamp": datetime.now().strftime("%b %d %Y %H:%M:%S")}
 
 
 @app.post("/predict_flower", response_model=QueryOut, status_code=200)
@@ -43,7 +45,7 @@ def ping():
 # Payload: QueryIn containing the parameters
 # Response: QueryOut containing the flower_class predicted (200)
 def predict_flower(query_data: QueryIn):
-    output = {"flower_class": predict(query_data)}
+    output = {"flower_class": predict(query_data), "timestamp": datetime.now().strftime("%b %d %Y %H:%M:%S")}
     return output
 
 @app.post("/feedback_loop", status_code=200)
@@ -52,7 +54,7 @@ def predict_flower(query_data: QueryIn):
 # Response: Dict with detail confirming success (200)
 def feedback_loop(data: List[FeedbackIn]):
     retrain(data)
-    return {"detail": "Feedback loop successful"}
+    return {"detail": "Feedback loop successful", "timestamp": datetime.now().strftime("%b %d %Y %H:%M:%S")}
 
 
 # Main function to start the app when main.py is called
