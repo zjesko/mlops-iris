fcd389c608d048220323cb39fda3825a3a77308f
AbinayaSankaran <abk050305@gmail.com>
2021-07-11 23:44:08 +0530

    [update] README and add -r

--- ml_utils.py
+++ ml_utils.py
@@ -2,27 +2,45 @@ from sklearn import datasets
 from sklearn.model_selection import train_test_split
 from sklearn.naive_bayes import GaussianNB
 from sklearn.metrics import accuracy_score
-
+from sklearn.linear_model import LogisticRegression
 # define a Gaussain NB classifier
-clf = GaussianNB()
-
+gaussian = GaussianNB()
+# define decision tree
+Log_reg = LogisticRegression(random_state=10)
 # define the class encodings and reverse encodings
 classes = {0: "Iris Setosa", 1: "Iris Versicolour", 2: "Iris Virginica"}
 r_classes = {y: x for x, y in classes.items()}
-
+#define a classifier to store 
+clf = gaussian
 # function to train and load the model during startup
 def load_model():
     # load the dataset from the official sklearn datasets
     X, y = datasets.load_iris(return_X_y=True)
 
-    # do the test-train split and train the model
+    # do the test-train split and train the model (guassian)
     X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2)
-    clf.fit(X_train, y_train)
+    gaussian.fit(X_train, y_train)
 
-    # calculate the print the accuracy score
-    acc = accuracy_score(y_test, clf.predict(X_test))
-    print(f"Model trained with accuracy: {round(acc, 3)}")
+    # do the test-train split and train the model (Decision Tree)
+    X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2,random_state=12)
+    Log_reg.fit(X_train, y_train)
 
+
+    # calculate the print the accuracy score (gaussian)
+    gaussian_acc = accuracy_score(y_test, gaussian.predict(X_test))
+    print(f"Model trained with accuracy for guassian: {round(gaussian_acc, 3)}")
+
+    # calculate the print the accuracy score (Decsion Tree)
+    log_acc = accuracy_score(y_test, Log_reg.predict(X_test))
+    print(f"Model trained with accuracy for logistic: {round(log_acc, 3)}")
+
+    #check for the two classifier performance and return the best
+    if gaussian_acc > log_acc :
+        clf = gaussian_acc
+        # print(clf)
+    else: 
+        clf = log_acc
+        #print(clf)
 
 # function to predict the flower using the model
 def predict(query_data):
--- test_app.py
+++ test_app.py
@@ -24,3 +24,34 @@ def test_pred_virginica():
         # asserting the correct response is received
         assert response.status_code == 200
         assert response.json() == {"flower_class": "Iris Virginica"}
+
+# TASK 2 : Adding two test to check the test case
+# test to check if Iris setosa is classified correctly
+def test_pred_setosa():
+    # defining a sample payload for the testcase
+    payload = {
+        "sepal_length": 5.1,
+        "sepal_width": 3.5,
+        "petal_length": 1.4,
+        "petal_width": 0.2,
+    }
+    with TestClient(app) as client:
+        response = client.post("/predict_flower", json=payload)
+        # asserting the correct response is received
+        assert response.status_code == 200
+        assert response.json() == {"flower_class": "Iris Setosa"}
+
+# test to check if Iris versicolor is classified correctly
+def test_pred_versicolor():
+    # defining a sample payload for the testcase
+    payload = {
+        "sepal_length": 6.2,
+        "sepal_width": 2.9,
+        "petal_length": 4.3,
+        "petal_width": 1.3,
+    }
+    with TestClient(app) as client:
+        response = client.post("/predict_flower", json=payload)
+        # asserting the correct response is received
+        assert response.status_code == 200
+        assert response.json() == {"flower_class": "Iris versicolor"}
