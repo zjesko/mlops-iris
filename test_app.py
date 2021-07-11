e98a79e49c0a5b60d1592c75c199f5be960cb9e3
gopi <ipogback@gmail.com>
2021-07-11 22:34:53 +0530

    [update] Added Two more test cases in test.py

--- test_app.py
+++ test_app.py
@@ -24,3 +24,33 @@ def test_pred_virginica():
         # asserting the correct response is received
         assert response.status_code == 200
         assert response.json() == {"flower_class": "Iris Virginica"}
+
+#Test cases added my Gopi
+
+def test_gopi1():
+    #defining sample payload for test case
+    payload = {
+        "sepal_length": 0,
+        "sepal_width": 0,
+        "petal_length": 1,
+        "petal_width": 0
+    }
+    with TestClient(app) as client:
+        response = client.post("/predict_flower",json=payload)
+        # confirming correct response is received
+        assert response.status_code == 200
+        assert response.json() == {"flower_class": "Iris Setosa"}
+
+def test_gopi2():
+    #defining sample payload for test case
+    payload = {
+        "sepal_length": 3,
+        "sepal_width": 3,
+        "petal_length": 2,
+        "petal_width": 1
+    }
+    with TestClient(app) as client:
+        response = client.post("/predict_flower",json=payload)
+        # confirming correct response is received
+        assert response.status_code == 200
+        assert response.json() == {"flower_class": "Iris Versicolour"}
