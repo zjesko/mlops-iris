e95e2373038a55a90e0eeeed8f582245da484ced
ranjit2784@gmail.com <ranjit2784.rg@gmail.com>
2021-07-11 16:12:55 +0530

    test

--- ml_utils.py
+++ ml_utils.py
@@ -9,7 +9,7 @@ from sklearn.metrics import accuracy_score
 
 # define a Gaussain NB classifier
 clf = GaussianNB()
-clf1 = MLPClassifier()
+clf1 = KNeighborsClassifier(n_neighbors=4)
 clf_better = clf
 
 # define the class encodings and reverse encodings
